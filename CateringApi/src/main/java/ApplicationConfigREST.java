60e389a65ca6af10ead28ee3d902af18973b61bd
Adrian Opiela <adrian.opiela11@gmail.com>
2019-05-16 12:40:37 +0200

    initial

---
+++ CateringApi/src/main/java/ApplicationConfigREST.java
@@ -0,0 +1,25 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest")
+public class ApplicationConfigREST extends Application {
+
+    private static final Set<Object> singletons  = new HashSet<Object>();
+
+    public ApplicationConfigREST() {
+//        singletons.add(new UserController());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> set = new HashSet<Class<?>>();
+        return set;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
