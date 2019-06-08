ad64b0681b7fc4d465fecac409385cbed0b27e24
kubas1129 <kubas11513@gmail.com>
2019-06-08 21:13:30 +0200

    message driven failed to jms get

--- CateringClient/src/main/java/bean/CateringMessageBean.java
+++ CateringClient/src/main/java/bean/CateringMessageBean.java
@@ -9,8 +9,7 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.jms.*;
 
-@ApplicationScoped
-@ManagedBean(name = "MesBean")
+
 @MessageDriven(name = "MyMDB",activationConfig = {
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/topic/OrderTopic"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
@@ -18,9 +17,6 @@ import javax.jms.*;
         @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "myTopicSubscription")
 })
 public class CateringMessageBean implements MessageListener {
-
-    private OrderJMS orderJMS;
-    private int count=0;
 
     public void onMessage(Message message) {
         try{
@@ -36,36 +32,7 @@ public class CateringMessageBean implements MessageListener {
     }
 
     public void processOrder(OrderJMS order){
-        System.out.println("PRYZPISANIE " + order.getAdidtionalInformation());
-        orderJMS = order;
-        System.out.println("PO PRZYP: " + orderJMS.getAdidtionalInformation());
+        System.out.println("ORDER JMS: " + order.getAdidtionalInformation());
     }
 
-    public void FilterOrderJMS(Long userId){
-        count++;
-        if(orderJMS != null){
-            System.out.println("WIADOM" + orderJMS.getAdidtionalInformation());
-//            if(orderJMS.getUserId() != userId) {
-//                orderJMS = null;
-//            }
-        }
-        System.out.println("FILTRACJA " + count);
-    }
-
-    public OrderJMS getOrderJMS() {
-        return orderJMS;
-    }
-
-    public void setOrderJMS(OrderJMS orderJMS) {
-        this.orderJMS = orderJMS;
-    }
-
-    public String GetOrderMessage(){
-        System.out.println("MEsSS ");
-        if(orderJMS != null){
-            return String.valueOf(orderJMS.getUserId());
-        }
-        else
-            return "";
-    }
 }
